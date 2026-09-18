ff489295d11284bd3bb3c41650f5feef3ebade81
Breno Rodrigues <brenofurtrodrigues@gmail.com>
2026-09-18 04:38:09 -0300

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,7 @@ public class OrderDTO {
     private ClientDTO client;
     private PaymentDTO payment;
 
+    @NotEmpty(message = "Deve ter pelo menos 1 item")
     private List<OrderItemDTO> items = new ArrayList<>();
 
     public OrderDTO() {
